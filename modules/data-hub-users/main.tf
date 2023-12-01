df476ecd85c209ad4c2968643113a509402eefd1
Scott Aubrey <scott@aubrey.org.uk>
2023-12-01 07:24:47 +0000

    make the resource name more descriptive

--- modules/data-hub-users/main.tf
+++ modules/data-hub-users/main.tf
@@ -6,7 +6,8 @@ locals {
     Project     = local.project_name
   }
 }
-resource "aws_iam_policy" "read_only" {
+
+resource "aws_iam_policy" "sciety_events_read_only" {
   name        = "${local.project_name}-sciety_events_read_only_access"
   path        = "/"
   description = "${local.project_name} read-only access to ${local.sciety_events_bucket_name} S3 Bucket"
@@ -43,5 +44,5 @@ resource "aws_iam_policy" "read_only" {
 
 resource "aws_iam_group_policy_attachment" "read-write-role-policy-attachment" {
   group      = var.data_hub_user_group_name
-  policy_arn = aws_iam_policy.read_only.arn
+  policy_arn = aws_iam_policy.sciety_events_read_only.arn
 }
